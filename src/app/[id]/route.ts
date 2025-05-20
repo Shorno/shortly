48f6eaa4a25e7b31e581fc75a0760e38374d282a
Shorno <fb.shorno@gmail.com>
2025-05-20 21:31:59 +0600

    fix import

--- src/app/[id]/route.ts
+++ src/app/[id]/route.ts
@@ -1,16 +1,13 @@
 import {type NextRequest} from 'next/server'
-import {prisma} from "@/lib/prisma";
 import {redirect} from "next/navigation";
+import {db} from "@/db";
 
 
 export async function GET(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
     const {id} = await params;
-    const existingLink = await prisma.link.findUnique({
-        where: {
-            generatedID: id
-        }
+    const existingLink = await db.query.links.findFirst({
+        where: (links, {eq}) => eq(links.generatedID, id)
     })
-
 
     if (!existingLink) {
         return new Response('Not found', {status: 404});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,6 +3,6 @@ import {integer, pgTable, varchar} from "drizzle-orm/pg-core";
 export const links = pgTable("links", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     originalURL: varchar({length: 500}).notNull(),
-    shortURL: varchar({length: 255}).notNull(),
-    generatedID: varchar({length: 50}).notNull(),
+    shortURL: varchar({length: 255}).notNull().unique(),
+    generatedID: varchar({length: 50}).notNull().unique(),
 })
