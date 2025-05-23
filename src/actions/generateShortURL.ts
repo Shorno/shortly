e45e7cb9801445c40ecb0765065b7f7be83a7823
Shorno <fb.shorno@gmail.com>
2025-05-24 02:45:12 +0600

    update page behaviour

--- src/actions/generateShortURL.ts
+++ src/actions/generateShortURL.ts
@@ -25,7 +25,7 @@ export default async function GenerateShortURL(originalURL: string) {
 
     try {
         await db.insert(links).values(data)
-        revalidatePath("/")
+        revalidatePath("/links")
         return {success: true}
 
     } catch (error: unknown) {
--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -2,6 +2,9 @@ import {Suspense} from "react";
 import LinksLoading from "@/components/links-loading";
 import Links from "@/components/links";
 
+export const dynamic = "force-dynamic"
+
+
 export default function LinksPage() {
     return (
         <div className={"flex justify-center pt-10"}>
