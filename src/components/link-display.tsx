54a2fea16fac3dc5071581026d7dc3eee6418c40
Shorno <fb.shorno@gmail.com>
2025-05-25 22:25:40 +0600

    fix card responsive

--- src/components/link-display.tsx
+++ src/components/link-display.tsx
@@ -11,14 +11,15 @@ export default function LinkDisplay({original_url, short_url, site_title, site_f
                 <Image src={site_favicon} alt={site_title} width={24} height={24}
                        className={"rounded-full object-cover  min-w-8 min-h-8 max-w-8 max-h-8  hidden md:flex"}/>
             </div>
-            <div className={"flex flex-col gap-1 px-2"}>
+            <div className={"flex flex-col gap-1"}>
                 <p className={"font-semibold"}>{site_title}</p>
                 <Link className={"font-semibold text-blue-500"} href={short_url}
                       target={"_blank"}>{short_url}
                 </Link>
                 <div className={"flex flex-col"}>
-                    <Link className={"text-sm"} href={original_url}
-                          target={"_blank"}>{original_url}</Link>
+                    <Link className={"text-sm break-all"} href={original_url}
+                          target={"_blank"}>{original_url}
+                    </Link>
                 </div>
                 <Separator className={"mt-2"}/>
                 <div>
