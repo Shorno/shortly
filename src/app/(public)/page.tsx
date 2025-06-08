4033883c073e5835c35f5ad50060df985430908b
Shorno <fb.shorno@gmail.com>
2025-06-09 01:36:50 +0600

    get metadata form external api

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,6 @@
 import LinkForm from "@/components/link-form";
-export default async function Home() {
 
+export default async function Home() {
     return (
         <div className={"flex justify-center pt-10 lg:pt-32 items-center flex-col"}>
             <LinkForm/>
--- src/utils/getMetadata.ts
+++ src/utils/getMetadata.ts
@@ -1,8 +1,6 @@
 "use server"
 export default async function getMetadata(originalLink: string) {
-    const hostname = new URL(originalLink).hostname;
-    const favicon = `https://www.google.com/s2/favicons?domain=${hostname}&sz=64`;
-    const response = await fetch(`${process.env.BASE_URL as string}/api/metadata?url=${originalLink}`);
-    const { title } = await response.json();
-    return { title, favicon };
+    const response = await fetch(`${process.env.METADATA_URL as string}?url=${originalLink}`);
+    const {title, favicon} = await response.json();
+    return {title, favicon};
 }
