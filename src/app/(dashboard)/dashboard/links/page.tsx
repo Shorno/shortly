bd7656776c9209bb91ab465cdc6f0e101e0910c2
Shorno <fb.shorno@gmail.com>
2025-06-11 02:52:10 +0600

    use redis cache

--- src/app/(dashboard)/dashboard/links/page.tsx
+++ src/app/(dashboard)/dashboard/links/page.tsx
@@ -11,13 +11,13 @@ export const metadata = {
 export default async function MyLinksPage() {
     const response = await GetUserURLs();
 
-    if (response.status === 401) return <div className={"mt-32"}><UnauthorizedCard/></div>
+    if (response?.status === 401) return <div className={"mt-32"}><UnauthorizedCard/></div>
 
     return (
         <div className={"flex justify-center pt-10"}>
 
             <Suspense fallback={<LinksLoading/>}>
-                <Links links={response.data}/>
+                <Links links={response?.data}/>
             </Suspense>
         </div>
     )
