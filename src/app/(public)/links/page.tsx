6a3084a2d4473eeec1bd52f0748fba2a47c36e67
Shorno <fb.shorno@gmail.com>
2025-06-12 13:08:04 +0600

    show loading between pagination

--- src/app/(public)/links/page.tsx
+++ src/app/(public)/links/page.tsx
@@ -3,6 +3,7 @@ import LinksLoading from "@/components/link/links-loading"
 import Links from "@/components/link/links"
 import GetPublicURLs from "@/data/getPublicURLs"
 import LinksPagination from "@/components/LinksPagination"
+import {nanoid} from "nanoid";
 
 export const metadata = {
     title: "Links",
@@ -23,11 +24,13 @@ export default async function LinksPage({
         pageSize: pageSize,
     })
 
+    console.log(paginatedLinks)
+
     const totalPages = Math.ceil(totalCount / pageSize)
 
     return (
         <div className={"flex justify-center flex-col gap-10 items-center mx-auto pt-32"}>
-            <Suspense fallback={<LinksLoading/>}>
+            <Suspense fallback={<LinksLoading/>} key={nanoid(4)}>
                 <Links links={paginatedLinks}/>
             </Suspense>
             <LinksPagination currentPage={currentPage} totalPages={totalPages} totalItems={totalCount}
